fd2910ac4f8d84551e9eb3a6cc760d3cc9a22ff6
杨铭 <webming@bitkan.com>
2022-02-11 10:40:37 +0800

    update base fundation structure

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+    ['@babel/preset-typescript']
+  ],
+  plugins: [
+    "babel-plugin-transform-typescript-metadata",
+    ["@babel/plugin-proposal-decorators", { "legacy": true }],
+    ["@babel/plugin-proposal-class-properties", { "loose" : true }],
+    ["@babel/plugin-transform-typescript", { "allowNamespaces": true }],
+    ['@babel/plugin-transform-runtime']
+  ]
+}
